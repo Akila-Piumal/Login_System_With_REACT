d808c27f79d7f3306694cfdb950340bb1025b4ab
Akila Piumal <akilapiumal16@gmail.com>
2023-04-30 13:58:44 +0530

    footer component added

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer-outer'>
+        <p className='cr-stm'>Copyright to Akila - 2023</p>
+        <p className='cr-stm'>All Rights Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
