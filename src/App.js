bbde677c8e4973e3203ce51f4fbf4d6d1ece96cd
Akila Piumal <akilapiumal16@gmail.com>
2023-04-30 13:57:10 +0530

    Login component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/nav/Nav';
+import Login from './components/login/Login';
 
 function App() {
   return (
     <div className="App">
         <Nav/>
+        <Login/>
     </div>
   );
 }
