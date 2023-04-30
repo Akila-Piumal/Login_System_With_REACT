9d90e091265847ec41c3b30fb64280cb9f76e9bb
Akila Piumal <akilapiumal16@gmail.com>
2023-04-30 16:31:26 +0530

    Signup component added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/nav/Nav';
 import Login from './components/login/Login';
+import Footer from './components/footer/Footer';
+import SignUp from './components/signup/Signup';
 
 function App() {
   return (
     <div className="App">
         <Nav/>
         <Login/>
+        <Footer/>
+        {/* <SignUp/> */}
     </div>
   );
 }
