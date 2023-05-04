f444a659d3d8773d13dcca7cd878b3be9de2dc3f
Akila Piumal <akilapiumal16@gmail.com>
2023-05-04 14:49:06 +0530

    DataFormCommon component added

---
+++ src/components/common/DataFormCommon.jsx
@@ -0,0 +1,21 @@
+import { TextField } from '@mui/material'
+import React from 'react'
+
+const DataFormCommon = (props) => {
+  return (
+    <div>
+        <TextField
+            className={props.className}
+            label={props.label}
+            variant={props.variant}
+            type={props.type}
+            value={props.value}
+            onChange={e=>{props.onChange(e.target.value)}}
+            required={props.required}
+        >
+        </TextField>        
+    </div>
+  )
+}
+
+export default DataFormCommon
